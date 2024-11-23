1ea44073cc1282b79109a2b67b63ac4e49167432
jcastiblancoc <castiblanco.jeisson@gmail.com>
2024-11-22 22:38:22 -0500

    Add cli.js file

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+
+const greetUserName = () => {
+  console.log('¡Bienvenido a Brain Games!');
+  const name = readlineSync.question('¿Cuál es tu nombre? ');
+  console.log(`¡Hola, ${name}!`);
+};
+
+export default greetUserName;
